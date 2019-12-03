a3d99e54b6b57ecc7df391a9be24058ee1e98633
Gwapa Ako Kunoh <nylael.nilue18181919@gmail.com>
2019-12-03 21:49:53 +0800

    Socket io nalang ang kulang

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', () => { /* … */ });
-server.listen(3000);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 
+var list_users = {};
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/login.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('message', function (msg) {
+        io.emit('message', msg);
+        console.log(msg)
+    });
+});
+
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
+
+var numUsers = 0;
+
+io.on('connection', (socket) => {
+    console.log("New Student is conected!");
+
+    socket.id = 11;
+
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+        if (!list_users.hasOwnProperty(data)) {
+            socket.username = data.username;
+            list_users[socket.username] = { online: true };
+            console.log('a user connected ' + socket.username);
+            io.sockets.emit('change_username', list_users);
+            console.log(socket.username)
+            console.log(list_users);
+        }
+    })
+
+
+
+
+
+
+
+});
